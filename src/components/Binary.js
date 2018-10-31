d2fd073b93f9e5f6d8bb0429fc35b91437e17fb2
Edward Timmer <edtimmer@gmail.com>
2018-10-31 17:15:07 -0400

    fixing calculation bugs in the binary display

--- src/components/Binary.js
+++ src/components/Binary.js
@@ -6,22 +6,33 @@ class Binary extends Component {
     const { a, b, result } = this.props;
     const newA = a[0] === '-' ? a.substr(1, 10) : a.substr(0, 10);
     const newB = b[0] === '-' ? b.substr(1, 10) : b.substr(0, 10);
-    const newResult = result === '' ? '' : (Math.abs(parseFloat(this.props.result)));
-    const newResultSafe = newResult.toString().substr(0, 10);
+    const newResult = result[0] === '-' ? result.substr(1, 10) : result.substr(0, 10);
+
+
+    const newResultSafe = result === '' ? '' : parseInt(newResult);
+    // const newResultSafe = newResult ? newResult.toString().substr(0, 10) : '';
+
+    const resultA = converter.convertToBinary(newA);
+    const resultB = converter.convertToBinary(newB);
+    // const resultResult = converter.convertToBinary(newResultSafe);
+    const convertedResult = parseInt(newResultSafe, 10).toString(2);
+    // const filteredResult = converter.convertToBinary(newResultSafe);
+    // const filteredNewResult = !!converter.convertToBinary(newResultSafe) ? converter.convertToBinary(newResultSafe) : '';
+    const displayResult = result === '' ? '' : convertedResult;
 
     if (this.props.on) {
       return (
         <div style={{ textAlign: 'center' }}>
           <span style={{ color: 'white' }}>
-            {converter.convertToBinary(newA)}
+            {resultA}
           </span>
           <br />
           <span style={{ color: 'white' }}>
-            {converter.convertToBinary(newB)}
+            {resultB}
           </span>
           <br />
           <span style={{ color: '#6dff8d' }}>
-            {converter.convertToBinary(newResultSafe)}
+            {displayResult}
           </span>
         </div>
 
